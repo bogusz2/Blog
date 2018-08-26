a6942f82ed10b3f32ff1648948708cee6a0ff00a
bogusz2 <boguszewicz@mail.ru>
2018-08-26 12:04:29 +0200

    Add image as MultiPartFile and Path for image; Add validation for title and text

--- src/main/java/boguszGroup/Blog/Post.java
+++ src/main/java/boguszGroup/Blog/Post.java
@@ -1,20 +1,50 @@
 package boguszGroup.Blog;
 
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 public class Post {
+  @NotNull
+  @Size(min=2, max=50)
   private String title;
+  @NotNull
+  @Size(min=1)
   private String text;
   private LocalDateTime time;
   private int id;
+  private MultipartFile img;
+  private String pathPostImage;
+
   public Post(String title, String text) {
     this.title = title;
     this.text = text;
     this.time = LocalDateTime.now();
+
   }
 
   public Post() {
+  }
+
+  public String getPathPostImage() {
+    return pathPostImage;
+  }
+
+  public void setPathPostImage(String pathPostImage) {
+    this.pathPostImage = pathPostImage;
+  }
+
+
+  public MultipartFile getImg() {
+    return img;
+  }
+
+  public void setImg(MultipartFile img) {
+    this.img = img;
   }
 
   public int getId() {

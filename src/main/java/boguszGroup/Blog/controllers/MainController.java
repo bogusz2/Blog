4d11925c118a6692d33310c11fbc66ab7c1f88aa
bogusz2 <boguszewicz@mail.ru>
2018-08-26 12:07:43 +0200

    Add image to post; Add validation for post

--- src/main/java/boguszGroup/Blog/controllers/MainController.java
+++ src/main/java/boguszGroup/Blog/controllers/MainController.java
@@ -2,6 +2,7 @@ package boguszGroup.Blog.controllers;
 
 import boguszGroup.Blog.Post;
 import boguszGroup.Blog.services.PostService;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,9 +10,12 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.time.format.DateTimeFormatter;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -39,18 +43,39 @@ public class MainController {
   }
 
   @RequestMapping("/post")
-  public String getPost(@RequestParam("id") Integer id, Model model){
+  public String getPost(@RequestParam("id") Integer id, Model model, HttpServletResponse response) throws IOException {
     Post post = postService.getPost(id);
     model.addAttribute("post", post);
-    model.addAttribute("id",post.getId());
+    model.addAttribute("id", post.getId());
+    //model.addAttribute("image",post.getBase64Image());
+
     return "post";
   }
 
+  @RequestMapping(value = "/postImageJPG")
+  public @ResponseBody
+  byte[] getPostImage(@RequestParam("id") Integer id, HttpServletResponse response, Model model) throws IOException {
+    model.addAttribute("id", id);
+    FileInputStream file = new FileInputStream(postService.getPost(id).getPathPostImage());
+    System.out.println(postService.getPost(id).getPathPostImage());
+    // InputStream in = getClass().getResourceAsStream(pathOfImage);
+    return IOUtils.toByteArray(file);
+  }
+
+
   @RequestMapping(value = "/blog", method = RequestMethod.POST)
-  public String savePost(Post post) {
-    System.out.println("dodaje wpis");
+  public String savePost(@Valid Post post, BindingResult bindingResult) {
+
+    if (bindingResult.hasErrors()) {
+      System.out.println("ERROR");
+      bindingResult.getAllErrors().forEach(error -> System.out.println(error.getObjectName() + " " + error.getDefaultMessage()));
+      return "nowywpis";
+    } else {
       postService.addPost(post);
       return "redirect:/blog";
+    }
 
   }
+
+
 }

55d3779d868ab2abc5dcf82583cdf036558d19c3
bogusz2 <boguszewicz@mail.ru>
2018-09-05 14:38:20 +0200

    call method whitch add posts from disc to h2 database

---
+++ src/main/java/boguszGroup/Blog/Starter.java
@@ -0,0 +1,24 @@
+package boguszGroup.Blog;
+
+
+import boguszGroup.Blog.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Scope("singleton")
+public class Starter implements CommandLineRunner {
+
+  @Autowired
+  PostRepository postRepository;
+
+
+  @Override
+  @Transactional
+  public void run(String... args) throws Exception {
+    postRepository.addPostsFromLocalDisc();
+  }
+}

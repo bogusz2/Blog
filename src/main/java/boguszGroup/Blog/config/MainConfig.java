bd65c17e0155dfcbf530b2857d193607857cf2df
bogusz2 <boguszewicz@mail.ru>
2018-09-05 14:47:32 +0200

    add 2 beans PostRepo

--- src/main/java/boguszGroup/Blog/config/MainConfig.java
+++ src/main/java/boguszGroup/Blog/config/MainConfig.java
@@ -1,15 +1,25 @@
 package boguszGroup.Blog.config;
 
-import boguszGroup.Blog.repository.PostRepository;
+import boguszGroup.Blog.repository.DBPostRepository;
+import boguszGroup.Blog.repository.InMemoryPostRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class MainConfig {
 
-  @Bean
-  public PostRepository createPostRepo(){
-    PostRepository postRepo = new PostRepository();
+  @Bean(name="inmemoryrepository")
+  @Profile("dev")
+  public InMemoryPostRepository createPostRepoInMem(){
+    InMemoryPostRepository postRepo = new InMemoryPostRepository();
+    return postRepo;
+  }
+
+  @Bean(name="DBRepository")
+  @Profile("prod")
+  public DBPostRepository createPostRepoInDB(){
+    DBPostRepository postRepo = new DBPostRepository();
     return postRepo;
   }
 }

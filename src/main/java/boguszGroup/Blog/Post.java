6f9a1ec614edd693064caa90130849fb97c5cea4
bogusz2 <boguszewicz@mail.ru>
2018-09-05 14:47:07 +0200

    add byteArrayImg; add Transient elements

--- src/main/java/boguszGroup/Blog/Post.java
+++ src/main/java/boguszGroup/Blog/Post.java
@@ -1,47 +1,43 @@
 package boguszGroup.Blog;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Lob;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+
 @Entity
-public @Data class Post {
+public @Data
+class Post {
 
   @NotNull
-  @Size(min=2, max=50)
   private String title;
 
   @Lob
   @NotNull
-  @Size(min=1)
+  @Size(min = 1)
   private String text;
 
   private String time;
-  
+
   @Id
   private int id;
 
-  @Lob
+  @Transient
   private MultipartFile img;
+
   @Lob
+  private byte[] byteArrayImg;
+
+  @Transient
   private String pathPostImage;
 
   public Post(String title, String text) {
     this.title = title;
     this.text = text;
-
-
   }
 
   public Post() {

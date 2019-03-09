77f6e0b59b939b32e032f86cc141afdae9cdc673
bogusz2 <boguszewicz@mail.ru>
2019-03-09 12:38:40 +0100

    Refactor

--- src/main/java/boguszGroup/Blog/controllers/MainController.java
+++ src/main/java/boguszGroup/Blog/controllers/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.IOException;
@@ -18,56 +19,56 @@ import java.util.List;
 @Controller
 public class MainController {
 
-  @Autowired
-  PostService postService;
+    @Autowired
+    PostService postService;
 
-  @RequestMapping({"/", "/start"})
-  public String start() {
-    return "home";
-  }
-
-  @RequestMapping("/blog")
-  public String blog(Model model) {
-    List<Post> allPosts = postService.getAllPosts();
-    model.addAttribute("posts", allPosts);
-    return "blog";
-  }
-
-  @RequestMapping("/dodajwpis")
-  public String nowywpis(Model model) {
-    model.addAttribute("post", new Post());
-    return "nowywpis";
-  }
-
-  @RequestMapping("/post")
-  public String getPost(@RequestParam("id") Long id, Model model, HttpServletResponse response) throws IOException {
-    Post post = postService.getPost(id);
-    model.addAttribute("post", post);
-    model.addAttribute("id", post.getId());
-
-
-    return "post";
-  }
-
-  @RequestMapping(value = "/postImageJPG")
-  public @ResponseBody
-  byte[] getPostImage(@RequestParam("id") Long id, HttpServletResponse response, Model model) throws IOException {
-    model.addAttribute("id", id);
-    return postService.getPost(id).getImg();
-  }
-
-
-  @RequestMapping(value = "/blog", method = RequestMethod.POST)
-  public String savePost(@Valid Post post, BindingResult bindingResult) {
-
-    if (bindingResult.hasErrors()) {
-      System.out.println("ERROR");
-      bindingResult.getAllErrors().forEach(error -> System.out.println(error.getObjectName() + " " + error.getDefaultMessage()));
-      return "nowywpis";
-    } else {
-      postService.addPost(post);
-      return "redirect:/blog";
+    @RequestMapping({"/", "/start"})
+    public String start() {
+        return "home";
     }
 
-  }
+    @RequestMapping("/blog")
+    public String blog(Model model) {
+        List<Post> allPosts = postService.getAllPosts();
+        model.addAttribute("posts", allPosts);
+        return "blog";
+    }
+
+    @RequestMapping("/dodajwpis")
+    public String newPost(Model model) {
+        model.addAttribute("post", new Post());
+        return "nowywpis";
+    }
+
+    @RequestMapping("/post")
+    public String getPost(@RequestParam("id") Long id, Model model, HttpServletResponse response) throws IOException {
+        Post post = postService.getPost(id);
+        model.addAttribute("post", post);
+        model.addAttribute("id", post.getId());
+
+
+        return "post";
+    }
+
+    @RequestMapping(value = "/postImageJPG")
+    public @ResponseBody
+    byte[] getPostImage(@RequestParam("id") Long id, HttpServletResponse response, Model model) throws IOException {
+        model.addAttribute("id", id);
+        return postService.getPost(id).getImg();
+    }
+
+
+    @RequestMapping(value = "/blog", method = RequestMethod.POST)
+    public String savePost(@Valid Post post, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            System.out.println("ERROR");
+            bindingResult.getAllErrors().forEach(error -> System.out.println(error.getObjectName() + " " + error.getDefaultMessage()));
+            return "nowywpis";
+        } else {
+            postService.addPost(post);
+            return "redirect:/blog";
+        }
+
+    }
 }
--- src/main/java/boguszGroup/Blog/repository/DBPostRepository.java
+++ src/main/java/boguszGroup/Blog/repository/DBPostRepository.java
@@ -1,15 +1,12 @@
 package boguszGroup.Blog.repository;
 
 import boguszGroup.Blog.Post;
-import org.apache.commons.io.IOUtils;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
@@ -17,50 +14,50 @@ import java.util.Collection;
 
 public class DBPostRepository implements PostRepository {
 
-  @PersistenceContext
-  private EntityManager em;
+    @PersistenceContext
+    private EntityManager em;
 
-  @Override
-  public Collection<Post> getPosts() {
-    return em.createQuery("from Post  order by id asc", Post.class).getResultList();
-  }
-
-  @Override
-  public Post getPostById(long id) {
-    return em.find(Post.class, id);
-  }
-
-  @Override
-  @Transactional
-  public void addNewPost(Post post) {
-    long maxId = em.createQuery("select max(id) from Post", Long.class).getSingleResult();
-    post.setId(maxId + 1);
-    post.setTime(LocalDateTime.now().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)));
-    try {
-      byte[] dataImg = post.getImgFile().getBytes();
-      post.setImg(dataImg);
-    } catch (IOException e) {
-      e.printStackTrace();
+    @Override
+    public Collection<Post> getPosts() {
+        return em.createQuery("from Post  order by id asc", Post.class).getResultList();
     }
 
-    em.persist(post);
-  }
-
-  @Transactional
-  @Override
-  public void addPostsFromLocalDisc(String folderPath) throws IOException {
-    File folder = new File(folderPath);
-    File[] fileList = folder.listFiles(file -> {
-      return file.getName().contains(".html");
-    });
-    if(fileList!=null) {
-      for (File src : fileList) {
-        em.persist(createPostFromLocalDir(src));
-      }
-      System.out.println("Dodałem posty z dysku do bazy danych");
-    }else{
-      System.out.println("Nie ma postów na dysku");
+    @Override
+    public Post getPostById(long id) {
+        return em.find(Post.class, id);
     }
-  }
+
+    @Override
+    @Transactional
+    public void addNewPost(Post post) {
+        long maxId = em.createQuery("select max(id) from Post", Long.class).getSingleResult();
+        post.setId(maxId + 1);
+        post.setTime(LocalDateTime.now().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)));
+        try {
+            byte[] dataImg = post.getImgFile().getBytes();
+            post.setImg(dataImg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        em.persist(post);
+    }
+
+    @Transactional
+    @Override
+    public void addPostsFromLocalDisc(String folderPath) throws IOException {
+        File folder = new File(folderPath);
+        File[] fileList = folder.listFiles(file -> {
+            return file.getName().contains(".html");
+        });
+        if (fileList != null) {
+            for (File src : fileList) {
+                em.persist(createPostFromLocalDir(src));
+            }
+            System.out.println("Dodałem posty z dysku do bazy danych");
+        } else {
+            System.out.println("Nie ma postów na dysku");
+        }
+    }
 
 }
